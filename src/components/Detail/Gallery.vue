918e0a5bb3c9d66de265a190a0bd2b654f70d9a7
ZhaiCR <ZhaiCongrui@163.com>
2020-01-16 13:50:21 +0800

    gallery部分

---
+++ src/components/Detail/Gallery.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="swiper-box">
+    <swiper :options="swiperOption" ref="mySwiper" v-if="detailGallery.length">
+      <!-- slides -->
+      <swiper-slide v-for="item of detailGallery" :key="item.id">
+        <img :src="item.url" :alt="item.name" />
+      </swiper-slide>
+      <!-- Optional controls -->
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DealGallery',
+  props: {
+    detailGallery: Array
+  },
+  data() {
+    return {
+      swiperOption: {
+        loop: true,
+        speed: 300, //切换到下一张图片所需的时间，默认300，可以省略不写
+        pagination: {
+          el: '.swiper-pagination',
+          type: 'fraction'
+        },
+        autoplay: {
+          delay: 3000,
+          /* 触摸滑动后是否禁止继续轮播 */
+          disableOnInteraction: false
+        }
+      }
+    }
+  },
+}
+</script>
+<style lang="styl" scoped>
+.swiper-box
+  position fixed
+  top 0
+  left 0
+  right 0
+  bottom 0
+  background black
+  z-index 10
+  .swiper-container
+    position absolute
+    top 50%
+    transform translateY(-50%)
+    height 6rem
+    img
+      width 100%
+</style>
--- src/views/Detail.vue
+++ src/views/Detail.vue
@@ -2,18 +2,47 @@
   <div class="detail-box">
     <detail-header></detail-header>
     <detail-banner></detail-banner>
+    <DetailGallery :detailGallery="gallery"></DetailGallery>
   </div>
 </template>
 
 <script>
 import DetailHeader from '@/components/Detail/Header'
 import DetailBanner from '@/components/Detail/Banner'
+import DetailGallery from '@/components/Detail/Gallery'
 
 export default {
   name: 'Detail',
+  data() {
+    return {
+      gallery: [
+        {
+          id: 1,
+          url: 'http://img1.qunarzz.com/sight/p0/1603/9b/9bf694a468dd774490.water.jpg_600x330_208523f8.jpg'
+        },
+        {
+          id: 2,
+          url: 'http://img1.qunarzz.com/sight/p0/1603/9b/9bf694a468dd774490.water.jpg_600x330_208523f8.jpg'
+        },
+        {
+          id: 3,
+          url: 'http://img1.qunarzz.com/sight/p0/1603/9b/9bf694a468dd774490.water.jpg_600x330_208523f8.jpg'
+        },
+        {
+          id: 4,
+          url: 'http://img1.qunarzz.com/sight/p0/1603/9b/9bf694a468dd774490.water.jpg_600x330_208523f8.jpg'
+        },
+        {
+          id: 5,
+          url: 'http://img1.qunarzz.com/sight/p0/1603/9b/9bf694a468dd774490.water.jpg_600x330_208523f8.jpg'
+        }
+      ]
+    }
+  },
   components: {
     DetailHeader,
-    DetailBanner
+    DetailBanner,
+    DetailGallery
   },
   created() {}
 }
